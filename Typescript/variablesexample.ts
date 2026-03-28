87a928fbde9aa7aea5c0ee9fd91e2019289b6fc7
prasanth <prasanthb313@gmail.com>
2026-03-28 12:29:24 +0530

    Fisrt Commit

---
+++ Typescript/variablesexample.ts
@@ -0,0 +1,32 @@
+
+
+// VariableName:typeofdata = value
+// let age: number = 28;
+// let name: string = "test automation";
+// let status: boolean = true;
+
+//cannot redeclare same variable
+//let age:
+
+/*
+    Type Annotation
+*/
+
+let distance = 30;
+console.log(typeof distance);
+
+
+let name = "test automation";
+console.log(typeof name);
+
+/*
+   Special Type: any (keyword)
+*/
+
+// let a: any = 10;
+// a ="test automation";
+// a = true;
+// a = 30;
+// a = "test";
+
+// console.log(a)
